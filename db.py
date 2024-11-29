fccec0f02c58213ae72406ed87ab7a7f1666eb8c
marialeang2 <ma.angulom1@uniandes.edu.co>
2024-11-29 18:08:44 -0500

    comentario db

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
 # MongoDB connection
+#Esta configurada con ip externa de GCP, probablemente toque cambiarla xd
 client = MongoClient('mongodb://34.45.86.253:27017/')
 db = client["instituciones_db"]
 collection = db["instituciones"]
