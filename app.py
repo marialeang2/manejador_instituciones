914f6ce8c3554400fecdbe7e612019d2543c112a
marialeang2 <ma.angulom1@uniandes.edu.co>
2024-12-05 07:45:11 -0500

    cambios nube en repo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from views import views
 
 app = Flask(__name__)
 
-app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(views, url_prefix='/instituciones')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, host='0.0.0.0',port=8080)
